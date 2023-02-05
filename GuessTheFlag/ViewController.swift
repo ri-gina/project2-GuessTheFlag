81d064849e81601f8629d6374d972e64bef7a720
Regina Mayorova <gina.rebrown@gmail.com>
2023-02-06 00:52:44 +0500

    Initial Commit

---
+++ GuessTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Regina Mayorova on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
